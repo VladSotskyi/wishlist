6efcc2540f6d5127c9450b211f1472838d16b4bf
Vlad Sotskyi <vlad@sotsky.com>
2024-07-20 17:34:05 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, {useState} from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [input, setInput] = useState("");
+
+  const addItem = () => {
+    if (input !== "") {
+      setItems(() => [...items, input]);
+      setInput("");
+    }
+  };
+
+  const deleteItem = (index) => {
+    setItems(items.filter((_, itemIndex) => itemIndex !== index));
+  };
+
+  const handleInputChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      addItem();
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App d-flex justify-content-center align-items-center">
+      <div className="p-4 rounded container">
+        <h1 className="mainTitle text-center">Wishlist</h1>
+        <input
+          className="wishItem mb-3"
+          type="text"
+          value={input}
+          onChange={handleInputChange}
+          onKeyDown={handleKeyPress}
+          placeholder="Do you want something ?"
+        />
+        <button className="wishItemButton btn mb-3" onClick={addItem}></button>
+        <ul className= 'listOfWishes list-group'>
+          {items.map((item, index) => (
+            <li key={index} className='list-group-item d-flex justify-content-between align-items-center listItem'>
+              <span>{index + 1}: {item}</span>
+              <button onClick={() => deleteItem(index)} className='btn wishItemDeleteButton'></button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
